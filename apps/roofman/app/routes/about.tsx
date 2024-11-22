cfacf8b2e18d26083bcf21b289e464c57e1b6e20
Edun Omatseye <edunomatseye@yahoo.com>
2024-11-22 20:05:30 +0100

    chore(core): added Tanstack Middleware that works for both client and server to the Server Function

--- apps/roofman/app/routes/about.tsx
+++ apps/roofman/app/routes/about.tsx
@@ -1,12 +1,20 @@
 import { createFileRoute, useRouter } from '@tanstack/react-router';
-import { createServerFn } from '@tanstack/start';
+import { createMiddleware, createServerFn } from '@tanstack/start';
 
 export const Route = createFileRoute('/')({
   component: Home,
   loader: async () => await getCount(),
 });
 
+export const logger = createMiddleware()
+  .validator((data: unknown) => data)
+  .server(async ({ next }) => {
+    console.log('a');
+    return next();
+  });
+
 const getMagicalBlackbok = createServerFn({ method: 'GET' })
+  .middleware([logger])
   .validator((data: unknown) => data)
   .handler(async ({ data, context, method }) => {
     const response = await fetch('https://placeholderdata.typicode.com/posts', {
